73ccc5e92846e985b2262b9f045e75118cfa81a6
TOL1990 <olefit.taras@mail.ru>
2016-03-25 07:59:42 +0200

    Task1 commit

--- app/src/main/java/task1/yalantis/com/task1/MainActivity.java
+++ app/src/main/java/task1/yalantis/com/task1/MainActivity.java
@@ -1,11 +1,16 @@
 package task1.yalantis.com.task1;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,21 +19,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         loadImages();
+
     }
 
     private void loadImages() {
 
-        ImageView img1  = (ImageView) findViewById(R.id.image1);
-        ImageView img2  = (ImageView) findViewById(R.id.image2);
-        Picasso.with(this).load("http://desktopgames.com.ua/games/23/manchkin-5f.jpg").into(img1);
-        Picasso.with(this).load("http://desktopgames.com.ua/games/23/manchkin-1f.jpg").into(img2);
+        ImageView img1 = (ImageView) findViewById(R.id.image1);
+        ImageView img2 = (ImageView) findViewById(R.id.image2);
+        Picasso.with(this).load(R.drawable.task1pict1).into(img1);
+        Picasso.with(this).load(R.drawable.task1pict2).into(img2);
 
     }
+
 
     @Override
     public void onBackPressed() {
-        Toast.makeText(this, "My custom text", Toast.LENGTH_SHORT).show();
 
-//        System.exit(0);
+        Toast.makeText(this, "Any control", Toast.LENGTH_SHORT).show();
+
+//       System.exit(0);
     }
+
+
 }
